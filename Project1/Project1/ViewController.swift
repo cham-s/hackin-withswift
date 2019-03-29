0ba4a8805c30aa87fcb1f29dbd683da4479b3a54
cham-s <cc.attou@gmail.com>
2019-03-29 04:51:07 +0100

    starting UI Design

--- Project1/Project1/ViewController.swift
+++ Project1/Project1/ViewController.swift
@@ -8,13 +8,22 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    
+    public var pictures: [String] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if item.hasPrefix("nssl") {
+                pictures.append(item)
+            }
+        }
     }
-
-
 }
 
