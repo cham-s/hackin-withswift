6c8fb3579a83cd734fa6b21a102ce68607be7d2c
cham-s <cc.attou@gmail.com>
2019-04-02 05:31:42 +0200

    replaced countries with flags

--- Challenge1/Challenge1/ViewController.swift
+++ Challenge1/Challenge1/ViewController.swift
@@ -10,27 +10,27 @@ import UIKit
 
 class ViewController: UITableViewController {
     
-    private var pictures: [String] = []
+    private var flags: [String] = []
 
     override func viewDidLoad() {
         let fm = FileManager.default
         let path = Bundle.main.resourcePath!
         let items = try! fm.contentsOfDirectory(atPath: path)
-        pictures = items.filter { $0.hasPrefix("nssl") }
-        print(pictures)
+        flags = items.filter { $0.hasSuffix("png") }.filter { $0.rangeOfCharacter(from: CharacterSet.decimalDigits) == nil }
+        print(flags)
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     
     override func tableView(_ tableView: UITableView,
                             numberOfRowsInSection section: Int) -> Int {
-        return pictures.count
+        return flags.count
     }
     
     override func tableView(_ tableView: UITableView,
                             cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
-        cell.textLabel?.text = pictures[indexPath.row]
+        cell.textLabel?.text = flags[indexPath.row]
         return cell
     }
 }
