a481a5c2f0cc6a7f6df365a117b8fad9b97ab6b9
cham-s <cc.attou@gmail.com>
2019-04-04 16:01:19 +0200

    proj6 so far

--- Project6b/Project6b/ViewController.swift
+++ Project6b/Project6b/ViewController.swift
@@ -12,9 +12,43 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let label1 = UILabel()
+        label1.translatesAutoresizingMaskIntoConstraints = false
+        label1.backgroundColor = UIColor.red
+        label1.text = "THESE"
+        label1.sizeToFit()
+        
+        let label2 = UILabel()
+        label2.translatesAutoresizingMaskIntoConstraints = false
+        label2.backgroundColor = UIColor.cyan
+        label2.text = "ARE"
+        label2.sizeToFit()
+        
+        let label3 = UILabel()
+        label3.translatesAutoresizingMaskIntoConstraints = false
+        label3.backgroundColor = UIColor.yellow
+        label3.text = "SOME"
+        label3.sizeToFit()
+        
+        let label4 = UILabel()
+        label4.translatesAutoresizingMaskIntoConstraints = false
+        label4.backgroundColor = UIColor.green
+        label4.text = "AWESOME"
+        label4.sizeToFit()
+        
+        let label5 = UILabel()
+        label5.translatesAutoresizingMaskIntoConstraints = false
+        label5.backgroundColor = UIColor.orange
+        label5.text = "LABELS"
+        label5.sizeToFit()
+        
+        let labels = [label1, label2, label3, label4, label5]
+        
+        labels.forEach { view.addSubview($0) }
     }
-
-
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
 }
 
