36e8f68657487b1209abe04675d16eb748352251
cham-s <cc.attou@gmail.com>
2019-04-05 18:09:45 +0200

    loading from api to 100

---
+++ Project7/Project7/Petition.swift
@@ -0,0 +1,15 @@
+//
+//  Petition.swift
+//  Project7
+//
+//  Created by chams on 04/04/2019.
+//  Copyright © 2019 Chams. All rights reserved.
+//
+
+import Foundation
+
+struct Petition: Codable {
+    public var title: String
+    public var body: String
+    public var signatureCount: Int
+}
--- Project7/Project7/ViewController.swift
+++ Project7/Project7/ViewController.swift
@@ -8,13 +8,50 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    
+    private var petitions: [Petition] = []
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let urlString = "https://api.whitehouse.gov/v1/petitions.json?limit=100"
+        
+        if let url = URL(string: urlString) {
+            do {
+                let data = try Data(contentsOf: url)
+                parse(json: data)
+            } catch {
+                print(error.localizedDescription)
+            }
+        }
     }
-
-
+    
+    private func parse(json: Data) {
+        let decoder = JSONDecoder()
+        print(json)
+        do {
+            let jsonPetitions = try decoder.decode(Petitions.self, from: json)
+            petitions = jsonPetitions.results
+            tableView.reloadData()
+        } catch {
+            print(error.localizedDescription)
+        }
+        
+    }
+    
+    override func tableView(_ tableView: UITableView,
+                            numberOfRowsInSection section: Int) -> Int {
+        return petitions.count
+    }
+    
+    override func tableView(_ tableView: UITableView,
+                            cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(
+            withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = "Title goes here"
+        return cell
+    }
 }
 
