be32f9c42e0b8954cf48dbae94dd0985d443db5a
cham-s <cc.attou@gmail.com>
2019-04-12 09:06:59 +0200

    spinning the slots

--- Project11/Project11/GameScene.swift
+++ Project11/Project11/GameScene.swift
@@ -18,16 +18,62 @@ class GameScene: SKScene {
         background.zPosition = -1
         addChild(background)
         
+        let numberOfBouncer = 5
+        let numberOfSlots = 4
+        (0...numberOfBouncer - 1).forEach {
+            makeBouncer(at: CGPoint(x: $0 * 256, y: 0))
+        }
+        
+        (0...numberOfSlots - 1).forEach {
+            let isGood = !($0 + 1).isMultiple(of: 2)
+            makeSlot(at: CGPoint(x: (128 + 256 * $0), y: 0), isGood: isGood)
+        }
+        
         physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
+    }
+    
+    func makeBouncer(at position: CGPoint) {
+        let bouncer = SKSpriteNode(imageNamed: "bouncer")
+        bouncer.position = position
+        bouncer.physicsBody = SKPhysicsBody(circleOfRadius:
+            bouncer.size.width / 2.0)
+        bouncer.physicsBody?.isDynamic = false
+        addChild(bouncer)
+    }
+    
+    func makeSlot(at position: CGPoint, isGood: Bool) {
+        var slotBase: SKSpriteNode
+        var slotGlow: SKSpriteNode
+        
+        if isGood {
+            slotBase = SKSpriteNode(imageNamed: "slotBaseGood")
+            slotGlow = SKSpriteNode(imageNamed: "slotGlowGood")
+        } else {
+            slotBase = SKSpriteNode(imageNamed: "slotBaseBad")
+            slotGlow = SKSpriteNode(imageNamed: "slotGlowBad")
+        }
+        
+        slotBase.position = position
+        slotGlow.position = position
+        
+        addChild(slotGlow)
+        addChild(slotBase)
+        
+        let spin = SKAction.rotate(byAngle: .pi, duration: 10)
+        let spinForever = SKAction.repeatForever(spin)
+        slotGlow.run(spinForever)
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         if let touch = touches.first {
             let location = touch.location(in: self)
-            let box = SKSpriteNode(color: UIColor.red, size: CGSize(width: 64, height: 64))
-            box.position = location
-            box.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 64, height: 64))
-            addChild(box)
+            
+            let ball = SKSpriteNode(imageNamed: "ballRed")
+            ball.physicsBody = SKPhysicsBody(circleOfRadius:
+                ball.size.width / 2.0)
+            ball.physicsBody?.restitution = 0.4
+            ball.position = location
+            addChild(ball)
         }
     }
 }
