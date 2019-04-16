f5e203b1232a8cdf8bc7d6a3e608a9c95c7ce8bf
cham-s <cc.attou@gmail.com>
2019-04-16 21:52:02 +0200

    loading image and  applying filter

--- Project13/Project13/ViewController.swift
+++ Project13/Project13/ViewController.swift
@@ -7,21 +7,69 @@
 //
 
 import UIKit
+import CoreImage
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,
+                      UIImagePickerControllerDelegate,
+                      UINavigationControllerDelegate {
 
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var intensity: UISlider!
     
+    public var currentImage: UIImage!
+    private var context: CIContext!
+    private var currentFilter: CIFilter!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        title = "YACIF"
+        navigationItem.rightBarButtonItem =
+            UIBarButtonItem(barButtonSystemItem: .add,
+                            target: self,
+                            action: #selector(importPicture))
+        context = CIContext()
+        currentFilter = CIFilter(name: "CISepiaTone")
+        
     }
+    
+    func imagePickerController(_ picker: UIImagePickerController,
+                               didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let image = info[.editedImage] as? UIImage else { return }
+        dismiss(animated: true)
+        currentImage = image
+        let beginImage = CIImage(image: currentImage)
+        currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
+        applyProcessing()
+    }
+    
+    @objc func importPicture() {
+        let picker = UIImagePickerController()
+        picker.allowsEditing = true
+        picker.delegate = self
+        present(picker, animated: true)
+    }
+    
+    func applyProcessing() {
+        
+        currentFilter.setValue(intensity.value, forKey: kCIInputIntensityKey)
+        
+        if let cgimg =
+        context.createCGImage(currentFilter.outputImage!,
+                              from: currentFilter.outputImage!.extent) {
+            let processedImage = UIImage(cgImage: cgimg)
+            imageView.image = processedImage
+        }
+    }
+    
     @IBAction func changeFilter(_ sender: UIButton) {
     }
     @IBAction func save(_ sender: UIButton) {
     }
     @IBAction func intensityChanged(_ sender: Any) {
+        applyProcessing()
     }
 }
 
