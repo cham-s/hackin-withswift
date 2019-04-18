8a787d389a5b1fec354bc97e2f5deba9a9e14df2
cham-s <cc.attou@gmail.com>
2019-04-18 22:44:34 +0200

    set up for different animations done

--- Project15/Project15/ViewController.swift
+++ Project15/Project15/ViewController.swift
@@ -9,12 +9,41 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var tap: UIButton!
+    private var imageView: UIImageView!
+    private var currentAnimation = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        imageView = UIImageView(image: UIImage(named: "penguin"))
+        imageView.center = CGPoint(x: 512, y: 384)
+        view.addSubview(imageView)
     }
 
-
+    @IBAction func tapped(_ sender: UIButton) {
+        
+        tap.isHidden = true
+        
+        UIView.animate(withDuration: 1, delay: 0, options: [], animations: { [unowned self] in
+            switch self.currentAnimation {
+            case 0:
+                break
+            default:
+                break
+            }
+            
+        }) { [unowned self] (finished: Bool) in
+            self.tap.isHidden = false
+        }
+        
+        currentAnimation += 1
+        
+        if currentAnimation > 7 {
+            currentAnimation = 0
+        }
+    }
+    
 }
 
