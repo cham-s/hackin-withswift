267457ad2468f0f70c1ed90220dffeb30326a129
cham-s <cc.attou@gmail.com>
2019-04-29 18:44:57 +0200

    auto finished

--- Project25/Project25/ViewController.swift
+++ Project25/Project25/ViewController.swift
@@ -7,11 +7,16 @@
 //
 
 import UIKit
+import MultipeerConnectivity
 
 class ViewController: UICollectionViewController, UINavigationControllerDelegate,
-                      UIImagePickerControllerDelegate {
+                      UIImagePickerControllerDelegate, MCSessionDelegate,
+                      MCBrowserViewControllerDelegate {
     
     private var images: [UIImage] = []
+    private var peerID: MCPeerID!
+    private var mcSession: MCSession!
+    private var mcAdvertiserAssistant: MCAdvertiserAssistant!
     
 
     override func viewDidLoad() {
@@ -21,6 +26,14 @@ class ViewController: UICollectionViewController, UINavigationControllerDelegate
         navigationItem.rightBarButtonItem =
             UIBarButtonItem(barButtonSystemItem: .camera, target: self,
                             action: #selector(importPicture))
+        navigationItem.leftBarButtonItem =
+            UIBarButtonItem(barButtonSystemItem: .add, target: self,
+                            action: #selector(showConnectionPrompt))
+        
+        peerID = MCPeerID(displayName: UIDevice.current.name)
+        mcSession = MCSession(peer: peerID, securityIdentity: nil,
+                              encryptionPreference: .required)
+        mcSession.delegate = self
     }
     
     override func collectionView(_ collectionView: UICollectionView,
@@ -40,11 +53,72 @@ class ViewController: UICollectionViewController, UINavigationControllerDelegate
             return cell
     }
     
+    func session(_ session: MCSession, peer peerID: MCPeerID,
+                 didChange state: MCSessionState) {
+        switch state {
+        case MCSessionState.connected:
+            print("Connected: \(peerID.displayName)")
+        case MCSessionState.connecting:
+            print("Connecting \(peerID.displayName)")
+        case MCSessionState.notConnected:
+            print("Not Connected: \(peerID.displayName)")
+        }
+    }
+    
+    func session(_ session: MCSession,
+                 didReceive data: Data, fromPeer peerID: MCPeerID) {
+        if let image = UIImage(data: data) {
+            DispatchQueue.main.async { [unowned self] in
+                self.images.insert(image, at: 0)
+                self.collectionView.reloadData()
+            }
+        }
+    }
+    
+    func session(_ session: MCSession, didReceive stream: InputStream,
+                 withName streamName: String, fromPeer peerID: MCPeerID) {
+        
+    }
+    
+    func session(_ session: MCSession,
+                 didStartReceivingResourceWithName resourceName: String,
+                 fromPeer peerID: MCPeerID, with progress: Progress) {
+        
+    }
+    
+    func session(_ session: MCSession,
+                 didFinishReceivingResourceWithName resourceName: String,
+                 fromPeer peerID: MCPeerID, at localURL: URL?,
+                 withError error: Error?) {
+        
+    }
+    
+    func browserViewControllerDidFinish(_ browserViewController:
+    MCBrowserViewController) {
+        dismiss(animated: true)
+    }
+    
+    func browserViewControllerWasCancelled(_ browserViewController:
+        MCBrowserViewController) {
+        dismiss(animated: true)
+    }
+    
     @objc func importPicture() {
         let picker = UIImagePickerController()
         picker.allowsEditing = true
         picker.delegate = self
         present(picker, animated: true)
+    }
+    
+    @objc func showConnectionPrompt() {
+        let ac = UIAlertController(title: "Connect to others", message: nil,
+                                   preferredStyle: .actionSheet)
+        ac.addAction(UIAlertAction(title: "Host a seesion",
+                                   style: .default, handler: startHosting))
+        ac.addAction(UIAlertAction(title: "Join a seesion",
+                                   style: .default, handler: joinSession))
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        present(ac, animated: true)
     }
     
     func imagePickerController(_ picker: UIImagePickerController,
@@ -54,6 +128,37 @@ class ViewController: UICollectionViewController, UINavigationControllerDelegate
         dismiss(animated: true)
         images.insert(image, at: 0)
         collectionView?.reloadData()
+        if mcSession.connectedPeers.count > 0 {
+            if let imageData = image.pngData() {
+                do {
+                    try mcSession.send(imageData,
+                                       toPeers: mcSession.connectedPeers,
+                                       with: .reliable)
+                } catch {
+                    let ac = UIAlertController(title: "Send error",
+                                               message: error.localizedDescription,
+                                               preferredStyle: .alert)
+                    ac.addAction(UIAlertAction(title: "OK", style: .default))
+                    present(ac, animated: true)
+                }
+            }
+        }
     }
+    
+    func startHosting(action: UIAlertAction) {
+        mcAdvertiserAssistant = MCAdvertiserAssistant(serviceType: "hws-project25",
+                                                      discoveryInfo: nil,
+                                                      session: mcSession)
+        mcAdvertiserAssistant.start()
+    }
+    
+    func joinSession(action: UIAlertAction) {
+        let mcBrowser = MCBrowserViewController(serviceType: "hws-project25",
+                                                session: mcSession)
+        mcBrowser.delegate = self
+        present(mcBrowser, animated: true)
+    }
+    
+    
 }
 
