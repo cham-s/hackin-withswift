c92fce1222aeca07cae4e3007c779fd979ed6292
cham-s <cc.attou@gmail.com>
2019-04-30 20:54:05 +0200

    player added to the scene

--- Project26/Project26/GameScene.swift
+++ Project26/Project26/GameScene.swift
@@ -20,12 +20,36 @@ enum CollisionTypes: UInt32 {
 
 class GameScene: SKScene {
     
+    private var player: SKSpriteNode!
+    
     override func didMove(to view: SKView) {
+        let background = SKSpriteNode(imageNamed: "background.jpg")
+        background.position = CGPoint(x: 512, y: 384)
+        background.blendMode = .replace
+        background.zPosition = -1
+        addChild(background)
         loadLevel()
+        createPlayer()
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         
+    }
+    
+    func createPlayer() {
+        player = SKSpriteNode(imageNamed: "player")
+        player.position = CGPoint(x: 96, y: 672)
+        player.physicsBody = SKPhysicsBody(circleOfRadius:
+            player.size.width / 2)
+        player.physicsBody?.allowsRotation = false
+        player.physicsBody?.linearDamping = 0.5
+        
+        player.physicsBody?.categoryBitMask = CollisionTypes.player.rawValue
+        player.physicsBody?.contactTestBitMask =
+            CollisionTypes.star.rawValue | CollisionTypes.vortex.rawValue |
+            CollisionTypes.wall.rawValue
+        player.physicsBody?.collisionBitMask = CollisionTypes.wall.rawValue
+        addChild(player)
     }
     
     func loadLevel() {
