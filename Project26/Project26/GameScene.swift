71b0ce4283dc59982fc93ba6507ca832dc2ba80c
cham-s <cc.attou@gmail.com>
2019-04-30 18:40:11 +0200

    added project 26 with resources

---
+++ Project26/Project26/GameScene.swift
@@ -0,0 +1,21 @@
+//
+//  GameScene.swift
+//  Project26
+//
+//  Created by chams on 30/04/2019.
+//  Copyright © 2019 Chams. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+}
