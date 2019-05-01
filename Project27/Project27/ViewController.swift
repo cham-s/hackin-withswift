ad279042c7eac5e3670825a620690272993d4a55
cham-s <cc.attou@gmail.com>
2019-05-01 22:12:09 +0200

    rectangle added

--- Project27/Project27/ViewController.swift
+++ Project27/Project27/ViewController.swift
@@ -10,14 +10,48 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private var currentDrawType = 0
+    
+    
     @IBOutlet weak var imageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        drawRectangle()
+    }
+    
+    func drawRectangle() {
+        let renderer = UIGraphicsImageRenderer(size: CGSize(width: 512,
+                                                            height: 512))
+        
+        let img = renderer.image { ctx in
+            let rectangle = CGRect(x: 0, y: 0, width: 512, height: 512)
+            
+            ctx.cgContext.setFillColor(UIColor.red.cgColor)
+            ctx.cgContext.setStrokeColor(UIColor.black.cgColor)
+            ctx.cgContext.setLineWidth(10)
+            
+            ctx.cgContext.addRect(rectangle)
+            ctx.cgContext.drawPath(using: .fillStroke)
+            
+        }
+        
+        imageView.image = img
     }
     
     @IBAction func redrawTapped(_ sender: Any) {
+        currentDrawType += 1
+        
+        if currentDrawType > 5 {
+            currentDrawType = 0
+        }
+        
+        switch currentDrawType {
+        case 0:
+            drawRectangle()
+        default:
+            break
+        }
     }
     
 }
