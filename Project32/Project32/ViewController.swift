595ff507466c756f3d1307aa35a27f0af4ea6da2
cham-s <cc.attou@gmail.com>
2019-05-07 19:52:36 +0200

    displaying tuto with safari view controller.

--- Project32/Project32/ViewController.swift
+++ Project32/Project32/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import SafariServices
 
 class ViewController: UITableViewController {
 
@@ -40,6 +41,10 @@ class ViewController: UITableViewController {
         return cell
     }
     
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        showTutorial(indexPath.row)
+    }
+    
     func makeAttributedString(title: String, subtitle: String) ->
         NSAttributedString {
             let titleAttributes = [NSAttributedString.Key.font: UIFont.preferredFont(forTextStyle: .headline),
@@ -51,6 +56,16 @@ class ViewController: UITableViewController {
             titleString.append(subtitleString)
             
             return titleString
+    }
+    
+    func showTutorial(_ which: Int) {
+        if let url = URL(string: "https://www.hackingwithswift.com/read/\(which + 1)") {
+            let config = SFSafariViewController.Configuration()
+            config.entersReaderIfAvailable = true
+            
+            let vc = SFSafariViewController(url: url, configuration: config)
+            present(vc, animated: true)
+        }
     }
 }
 
