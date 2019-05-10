3468157d32564c913b02b321115aee775c390026
cham-s <cc.attou@gmail.com>
2019-05-10 19:50:08 +0200

    ui finished

--- Project34bis/Project34bis/ViewController.swift
+++ Project34bis/Project34bis/ViewController.swift
@@ -10,11 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var columnButtons: [UIButton]!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    @IBAction func makeMove(_ sender: UIButton) {
+    }
 }
 
