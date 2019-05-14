82edde506ccddbf9498db776b2b92e646923d6d5
cham-s <cc.attou@gmail.com>
2019-05-14 20:13:00 +0200

    let's try to build four in a row with sprite kit!

---
+++ FourAligned/FourAligned/GameScene.swift
@@ -0,0 +1,14 @@
+//
+//  GameScene.swift
+//  FourAligned
+//
+//  Created by chams on 14/05/2019.
+//  Copyright © 2019 Chams. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+}
