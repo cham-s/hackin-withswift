6897acc402b97ec8b3bd21b0e6a3d347efa9b7f6
cham-s <cc.attou@gmail.com>
2019-05-14 20:33:39 +0200

    label configured.

--- FourAligned/FourAligned/GameScene.swift
+++ FourAligned/FourAligned/GameScene.swift
@@ -10,5 +10,27 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
+    private var currentPlayerLabel: SKLabelNode!
+    private var scoreLabel: SKLabelNode!
     
+    override func didMove(to view: SKView) {
+        
+    }
+    
+    func labelFor(text: String) -> SKLabelNode {
+        let label = SKLabelNode(fontNamed: "Chalkduster")
+        label.text = text
+        return label
+    }
+    
+    func configureLabels() {
+        currentPlayerLabel = labelFor(text: "Player's Turn: ")
+        currentPlayerLabel.position = CGPoint(x: 10, y: 760)
+        addChild(currentPlayerLabel)
+        
+        scoreLabel = labelFor(text: "Score: 0")
+        scoreLabel.horizontalAlignmentMode = .right
+        scoreLabel.position = CGPoint(x: 980, y: 700)
+        addChild(scoreLabel)
+    }
 }
